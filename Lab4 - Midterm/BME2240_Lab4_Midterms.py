f716180f7f4428f8ed92ba9d3be32d6ee6491d9f
ZefraSK <ryanlerisk@gmail.com>
2023-04-12 19:59:19 -0600

    Finished Lab4

--- Lab4 - Midterm/BME2240_Lab4_Midterms.py
+++ Lab4 - Midterm/BME2240_Lab4_Midterms.py
@@ -4,14 +4,24 @@ import matplotlib.pyplot as plt
 def equation(u_2,u_1):
     #Adjust these until they match
     g = -9.81 #m/s^2
-    c = .023#.002 #kg [.001]
-    k = 45.2#43 #n/m [45]
-    m = .7 #kg [.7]
+    c = .0158 #.0142 #.002 #kg [.001]
+    k = 51.9 #41.8 #43 #n/m [45]
+    m = .712 #.64 #kg [.7]
     
     #Equation Calculated from step 5
     u_2P = m*g - (c/m)*u_2 - (k/m)*u_1
 
     return u_2P
+
+def eq_given(u_2,u_1):
+    g = -9.81 #m/s^2
+    c_given =.001
+    k_given = 45
+    m_given = .7
+
+    u_2P_given = m_given*g - (c_given/m_given)*u_2 - (k_given/m_given)*u_1
+
+    return u_2P_given
 
 def rk4Method(xi,xf,u_2,u_1):
     # ------ Key ------- 
@@ -42,31 +52,51 @@ def rk4Method(xi,xf,u_2,u_1):
     #Creating lists for u_1,u_2 and k
     u_1_plot = []
     u_2_rk4 = []
+    u_1_plot_given = []
+    u_2_rk4_given = []
+
     k1_l = []
     k2_l = []
     k3_l = []
     k4_l = []
     newXPoints = []
-    
+
+    u_1_g = u_1
+    u_2_g = u_2
+
     #Iterating through points of x
     for i in xpoints:
         #Adding (u_1,u_2) to there respected list
         u_1_plot.append(u_1)
         u_2_rk4.append(u_2)
+        u_1_plot_given.append(u_1_g)
+        u_2_rk4_given.append(u_2_g)
         newXPoints.append(i+.5*h)
 
         #Solving for K-Values
         m1 = h*u_2
         k1 = h*equation(u_2,u_1)
 
+        m1_g = h*u_2_g
+        k1_g = h*eq_given(u_2_g,u_1_g)
+
         m2 = h*(u_2 + .5*k1)
         k2 = h*equation(u_2 + .5*k1, u_1 + .5*m1)
+
+        m2_g = h*(u_2_g + .5*k1_g)
+        k2_g = h*eq_given(u_2_g + .5*k1_g, u_1_g + .5*m1_g)
         
         m3 = h*(u_2 + .5*k2)
         k3 = h*equation(u_2 + .5*k2, u_1 + .5*m2)
 
+        m3_g = h*(u_2_g + .5*k2_g)
+        k3_g = h*eq_given(u_2_g + .5*k2_g, u_1_g + .5*m2_g)
+
         m4 = h*(u_2 + k3)
         k4 = h*equation(u_2 + k3, u_1 + m3)
+
+        m4_g = h*(u_2_g + k3_g)
+        k4_g = h*eq_given(u_2_g + k3_g, u_1_g + m3_g)
     
         #phi = ((1/6) * k1) + ((1/3) * k2) + ((1/3) * k3) + ((1/6) * k4)
         
@@ -80,6 +110,8 @@ def rk4Method(xi,xf,u_2,u_1):
         #Solving for Y-RK4 and X
         u_1 += (m1 + 2*m2 + 2*m3 + m4)/6
         u_2 += (k1+ 2*k2 + 2*k3 + k4)/6
+        u_1_g += (m1_g + 2*m2_g + 2*m3_g + m4_g)/6
+        u_2_g += (k1_g+ 2*k2_g + 2*k3_g + k4_g)/6
     
     '''
     #For Debugging
@@ -103,8 +135,9 @@ def rk4Method(xi,xf,u_2,u_1):
     print(u_1_plot)
 
     #Plotting all X_values and y_RK4 values
-    plt.plot(newXPoints,u_1_plot,color='r',label='RK4 Analysis')
-    plt.plot(t,l,color='g',label='Measured')
+    plt.plot(xpoints,u_1_plot,color='b',label='Analytical Solution RK4 Analysis')
+    plt.plot(xpoints,u_1_plot_given,color='r',label='RK4 approximation')
+    plt.plot(t,l,color='g',label='Experimental Data')
     plt.xlabel('Time (s)')
     plt.ylabel('Height (m)')
     plt.legend()
